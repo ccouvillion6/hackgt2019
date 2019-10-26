cdd3a42635b9aef4cbdbb06138f382c44b9af1a0
Laura-Neff <ljneff@gmail.com>
2019-10-26 12:46:07 -0400

    Added info to index.html

---
+++ javascript/apis.js
@@ -0,0 +1,9 @@
+var giphyURL = "https://api.giphy.com/v1/gifs/search?q=";
+
+var apiKey = config.API_KEY;
+var queryURL = giphyURL + dessert + apiKey;
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
