24a2feaa6ab4118771fda795249ffdaedc79b744
ccouvillion6 <ccouvillion6@gatech.edu>
2019-10-26 17:58:36 -0400

    Changed map to US only

--- main.js
+++ main.js
@@ -1,36 +1,23 @@
-//code ripped straight from Leaflet tutorials
+//The map container
+var container = d3.select("div");
 
 
-var accessToken = 'pk.eyJ1IjoiY2NvdXZpbGxpb242IiwiYSI6ImNrMjdwNGJ5eTAyOXIzb25yc3V6Mmtkcm8ifQ.1zs1ALhenQunRGELj8KVMg';
-var mymap = L.map('mapid').setView([37.8, -96], 4);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox.light',
-	accessToken: accessToken
-}).addTo(mymap);
-mymap.once('load', () => {
-	mymap.resize()
-  })
+// The svg
+var svg = d3.select("svg");
 
-// add json data to map
+var path = d3.geoPath();
 
-//highlight on mouseover
-function highlightFeature(e) {
-	var layer = e.target;
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
 
-	layer.setStyle({
-		weight: 5,
-		color: '#666',
-		dashArray: '',
-		fillOpacity: 0.7
-	});
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
 
-	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-		layer.bringToFront();
-	}
-}
-
-//un-highlight on mouseout
-function resetHighlight(e) {
-	//geojson.resetStyle(e.target);
-}
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+});
