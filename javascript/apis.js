fefaba8afbbd1cefc68af7396276eb4add282387
ccouvillion6 <ccouvillion6@gatech.edu>
2019-10-26 15:57:11 -0400

    Broken map

--- javascript/apis.js
+++ javascript/apis.js
@@ -1,9 +1,9 @@
 var giphyURL = "https://api.giphy.com/v1/gifs/search?q=";
 
-var apiKey = config.API_KEY;
-var queryURL = giphyURL + dessert + apiKey;
+//var apiKey = config.API_KEY;
+//var queryURL = giphyURL + dessert + apiKey;
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
+//$.ajax({
+//  url: queryURL,
+//  method: "GET"
+//})
--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 //code ripped straight from Leaflet tutorials
 
 
-var mapboxAccessToken = 'pk.eyJ1IjoiY2NvdXZpbGxpb242IiwiYSI6ImNrMjdwNGJ5eTAyOXIzb25yc3V6Mmtkcm8ifQ.1zs1ALhenQunRGELj8KVMg';
-var map = L.map('mapid').setView([37.8, -96], 4);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+var accessToken = 'pk.eyJ1IjoiY2NvdXZpbGxpb242IiwiYSI6ImNrMjdwNGJ5eTAyOXIzb25yc3V6Mmtkcm8ifQ.1zs1ALhenQunRGELj8KVMg';
+var mymap = L.map('mapid').setView([37.8, -96], 4);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	id: 'mapbox.light',
-	attribution: 'Mapbox'
-}).addTo(map);
+	accessToken: accessToken
+}).addTo(mymap);
+mymap.once('load', () => {
+	mymap.resize()
+  })
+
+// add json data to map
+
+//highlight on mouseover
+function highlightFeature(e) {
+	var layer = e.target;
+
+	layer.setStyle({
+		weight: 5,
+		color: '#666',
+		dashArray: '',
+		fillOpacity: 0.7
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+		layer.bringToFront();
+	}
+}
+
+//un-highlight on mouseout
+function resetHighlight(e) {
+	//geojson.resetStyle(e.target);
+}
